fd86c82ce7dda2d690e2d9ede32109680c8ea4b7
Josué Almeida <josue.almeida@gmail.com>
2021-05-16 17:32:24 +0100

    Created Header and PageLayout components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <div class="index-header">
+            <h4>Process Chemistry in the Pharmaceutical Industry: Key Concepts and Selected Examples</h4>
+        </div>
+    </header>
+);
+
+export default Header;
---
+++ src/components/PageLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/style.css';
+import Header from './Header';
+
+const PageLayout = () => (
+    <div class="main-wrapper">
+        <Header/>
+    </div>
+);
+
+export default PageLayout;
