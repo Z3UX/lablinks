b6d0887d03a5e14f1876f64eb243095cee563aba
Josué Almeida <josue.almeida@gmail.com>
2021-05-18 15:46:15 +0100

    Routing between articles finalized. Started bulding Related Items card

--- src/components/RelatedItems.js
+++ src/components/RelatedItems.js
@@ -13,7 +13,7 @@ const RelatedItems = ({relatedItems}) => {
     function getRelatedItemsID(relatedItems) {
         if (relatedItems) {
             let arr = JSON.parse("[" + relatedItems + "]");
-            
+
             return arr.map(id => pad_with_zeroes(id, 4));
         } else return [];
     }
@@ -21,19 +21,52 @@ const RelatedItems = ({relatedItems}) => {
     return (
         <div className="relatedItems">
             <h6>Related items</h6>
-            {getRelatedItemsID(relatedItems).map(id => (
-                <div className="relatedItemsCard" key={id}>
-                    <a href={id}><p>PLACEHOLDER {id}</p></a>
-                </div>
-            ))}
+            <div className="row">
+                {getRelatedItemsID(relatedItems).map(id => (
+                    <a href={id}>
+                        <div className="relatedItemsCard" key={id}>
+                            <div className="column">
+                                <div className="relatedItemThumbnail">
+                                    <img src="favicon.png" alt="Default image" width="150" height="150"/>
+                                </div>
+                            </div>
+                            <div className="column">
+                                <div className="relatedItemData">
+                                    <span className="relatedItemDesignation">PRESENTATION #{id}&nbsp;</span>
+                                    <span className="relatedItemName">Process Chemistry in the Pharmaceutical
+                                        Industry:
+                                        Key
+                                        Concepts and Selected Examples&nbsp;
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                ))}
+            </div>
 
             <style jsx>{`
+
+              .row {
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+                width: 100%;
+              }
+
+              .column {
+                display: flex;
+                flex-direction: column;
+                flex-basis: 100%;
+                flex: 1;
+                margin-bottom: 10px;
+              }
 
               .relatedItems {
                 position: absolute;
                 margin: 0 0 0 20px;
                 display: block;
-                width: 50%;
+                width: 100%;
               }
 
               .relatedItemsCard {
@@ -47,6 +80,14 @@ const RelatedItems = ({relatedItems}) => {
                 display: block;
               }
 
+              .relatedItemThumbnail {
+                float: left;
+              }
+
+              .relatedItemData {
+                display: inline-block;
+              }
+
               h6 {
                 font-size: 18px;
                 font-weight: 600;
@@ -55,6 +96,9 @@ const RelatedItems = ({relatedItems}) => {
                 margin: -15px 0 15px 10px;
               }
 
+              a {
+                text-decoration: none; /* no underline */
+              }
             `}</style>
         </div>
     );
